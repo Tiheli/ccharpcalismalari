4d69db0f89e9b64a9b21aae9a85c5be9bc47b753
Tiheli <toklusuleyman24@gmail.com>
2022-09-01 19:09:13 +0300

    ccharpcalismalari
     c# ile kullanıcıdan değer alma string veri tipi

---
+++ çalışmalarım_c/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace çalışmalarım_c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // readline ile kullanıcıdan değer alma
+            Console.WriteLine("kullanıcı ismi giriniz: ");
+
+            string isim = Console.ReadLine();
+
+            Console.WriteLine("kullanıcı soyismi giriniz: ");
+  
+            string soyisim = Console.ReadLine();
+            Console.WriteLine("kullanıcı ismi: " + isim);
+            Console.WriteLine("kullanıcı soysimi: " + soyisim);
+
+            Console.ReadLine();
+        }
+    }
+}
